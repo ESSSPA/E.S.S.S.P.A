b4cfcb8fea6237f9d5c72aa9acdcb4bdc932ea93
ESSSPA <77051407+ESSSPA@users.noreply.github.com>
2021-01-06 15:34:22 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff5899f7c0903001e324733",
+				"5ff5899f7c0903001e324735"
+			]
+		},
+		"5ff5899f7c0903001e324733": {
+			"id": "5ff5899f7c0903001e324733",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff5899f7c0903001e324735": {
+			"id": "5ff5899f7c0903001e324735",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
